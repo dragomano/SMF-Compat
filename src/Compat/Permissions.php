dec833eb6b6adbda27d9f06a0f4466d8e6dd574e
Bugo <cheetah@bk.ru>
2024-06-05 17:20:06 +0500

    Add a new class - Permissions

---
+++ src/Compat/Permissions.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+/**
+ * Permissions.php
+ *
+ * @package SMF Compat
+ * @link https://github.com/dragomano/smf-compat
+ * @author Bugo <bugo@dragomano.ru>
+ * @copyright 2024 Bugo
+ * @license https://opensource.org/license/mit/ MIT
+ *
+ * @version 0.1
+ */
+
+namespace Bugo\Compat;
+
+class Permissions
+{
+	public static array $permission_groups = [
+		'global' => [
+			'general',
+			'pm',
+			'calendar',
+			'maintenance',
+			'member_admin',
+			'profile',
+			'profile_account',
+			'likes',
+			'mentions',
+			'bbc',
+		],
+		'board' => [
+			'general_board',
+			'topic',
+			'post',
+			'poll',
+			'attachment',
+		],
+	];
+
+	public static array $left_permission_groups = [
+		'general',
+		'calendar',
+		'maintenance',
+		'member_admin',
+		'topic',
+		'post',
+	];
+}
--- src/compat.php
+++ src/compat.php
@@ -5,40 +5,41 @@ if (! defined('SMF_VERSION'))
 
 use Bugo\Compat\{ACP, BBCodeParser, Board, BoardIndex, CacheApi, Calendar, Config};
 use Bugo\Compat\{Db, Editor, ErrorHandler, IntegrationHook, ItemList};
-use Bugo\Compat\{Lang, Logging, Mail, Menu, MessageIndex, Msg, Notify, PageIndex};
-use Bugo\Compat\{Sapi, Security, ServerSideIncludes, Theme};
-use Bugo\Compat\{Topic, User, Utils, WebFetchApi};
+use Bugo\Compat\{Lang, Logging, Mail, Menu, MessageIndex, Msg, Notify};
+use Bugo\Compat\{PageIndex, Permissions, Sapi, Security, ServerSideIncludes};
+use Bugo\Compat\{Theme, Topic, User, Utils, WebFetchApi};
 
 if (str_starts_with(SMF_VERSION, '3.0')) {
 	$aliases = [
-		'SMF\\Actions\\Admin\\ACP'   => ACP::class,
-		'SMF\\Actions\\BoardIndex'   => BoardIndex::class,
-		'SMF\\Actions\\Calendar'     => Calendar::class,
-		'SMF\\Actions\\MessageIndex' => MessageIndex::class,
-		'SMF\\Actions\\Notify'       => Notify::class,
-		'SMF\\BBCodeParser'          => BBCodeParser::class,
-		'SMF\\Board'                 => Board::class,
-		'SMF\\Cache\\CacheApi'       => CacheApi::class,
-		'SMF\\Config'                => Config::class,
-		'SMF\\Db\\DatabaseApi'       => Db::class,
-		'SMF\\Editor'                => Editor::class,
-		'SMF\\ErrorHandler'          => ErrorHandler::class,
-		'SMF\\IntegrationHook'       => IntegrationHook::class,
-		'SMF\\ItemList'              => ItemList::class,
-		'SMF\\Lang'                  => Lang::class,
-		'SMF\\Logging'               => Logging::class,
-		'SMF\\Mail'                  => Mail::class,
-		'SMF\\Menu'                  => Menu::class,
-		'SMF\\Msg'                   => Msg::class,
-		'SMF\\PageIndex'             => PageIndex::class,
-		'SMF\\Sapi'                  => Sapi::class,
-		'SMF\\Security'              => Security::class,
-		'SMF\\ServerSideIncludes'    => ServerSideIncludes::class,
-		'SMF\\Theme'                 => Theme::class,
-		'SMF\\Topic'                 => Topic::class,
-		'SMF\\User'                  => User::class,
-		'SMF\\Utils'                 => Utils::class,
-		'SMF\\WebFetch\\WebFetchApi' => WebFetchApi::class,
+		'SMF\\Actions\\Admin\\ACP'         => ACP::class,
+		'SMF\\Actions\\Admin\\Permissions' => Permissions::class,
+		'SMF\\Actions\\BoardIndex'         => BoardIndex::class,
+		'SMF\\Actions\\Calendar'           => Calendar::class,
+		'SMF\\Actions\\MessageIndex'       => MessageIndex::class,
+		'SMF\\Actions\\Notify'             => Notify::class,
+		'SMF\\BBCodeParser'                => BBCodeParser::class,
+		'SMF\\Board'                       => Board::class,
+		'SMF\\Cache\\CacheApi'             => CacheApi::class,
+		'SMF\\Config'                      => Config::class,
+		'SMF\\Db\\DatabaseApi'             => Db::class,
+		'SMF\\Editor'                      => Editor::class,
+		'SMF\\ErrorHandler'                => ErrorHandler::class,
+		'SMF\\IntegrationHook'             => IntegrationHook::class,
+		'SMF\\ItemList'                    => ItemList::class,
+		'SMF\\Lang'                        => Lang::class,
+		'SMF\\Logging'                     => Logging::class,
+		'SMF\\Mail'                        => Mail::class,
+		'SMF\\Menu'                        => Menu::class,
+		'SMF\\Msg'                         => Msg::class,
+		'SMF\\PageIndex'                   => PageIndex::class,
+		'SMF\\Sapi'                        => Sapi::class,
+		'SMF\\Security'                    => Security::class,
+		'SMF\\ServerSideIncludes'          => ServerSideIncludes::class,
+		'SMF\\Theme'                       => Theme::class,
+		'SMF\\Topic'                       => Topic::class,
+		'SMF\\User'                        => User::class,
+		'SMF\\Utils'                       => Utils::class,
+		'SMF\\WebFetch\\WebFetchApi'       => WebFetchApi::class,
 	];
 
 	$applyAlias = static fn($class, $alias) => class_alias($class, $alias);
