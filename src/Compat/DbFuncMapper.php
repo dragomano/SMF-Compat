10695bbcd2b7de2fea79172443fa698298f24ca4
Bugo <cheetah@bk.ru>
2024-11-10 12:29:08 +0500

    Refactor DbFuncMapper

--- src/Compat/DbFuncMapper.php
+++ src/Compat/DbFuncMapper.php
@@ -18,18 +18,60 @@ use function array_key_exists;
 
 class DbFuncMapper
 {
-	public function __call(string $name, array $arguments)
+	public function query(string $identifier, string $db_string, array $db_values = []): object|bool
 	{
-		if ($name === 'search_query') {
-			$name = 'query';
-		}
+		return Utils::$smcFunc['db_query']($identifier, $db_string, $db_values);
+	}
 
-		$name = 'db_' . $name;
+	public function search_query(string $identifier, string $db_string, array $db_values = []): object|bool
+	{
+		return $this->query($identifier, $db_string, $db_values);
+	}
 
-		if (array_key_exists($name, Utils::$smcFunc)) {
-			return Utils::$smcFunc[$name](...$arguments);
-		}
+	public function fetch_row(object $result): array|false|null
+	{
+		return Utils::$smcFunc['db_fetch_row']($result);
+	}
 
-		return false;
+	public function fetch_assoc(object $result): array|false|null
+	{
+		return Utils::$smcFunc['db_fetch_assoc']($result);
+	}
+
+	public function fetch_all(object $request): array
+	{
+		return Utils::$smcFunc['db_fetch_all']($request);
+	}
+
+	public function free_result(object $result): bool
+	{
+		return Utils::$smcFunc['db_free_result']($result);
+	}
+
+	public function insert(
+		string $method,
+		string $table,
+		array $columns,
+		array $data,
+		array $keys,
+		int $returnmode = 0
+	): int|array|null
+	{
+		return Utils::$smcFunc['db_insert']($method, $table, $columns, $data, $keys, $returnmode);
+	}
+
+	public function num_rows(object $result): int
+	{
+		return Utils::$smcFunc['db_num_rows']($result);
+	}
+
+	public function transaction(string $type = 'commit'): bool
+	{
+		return Utils::$smcFunc['db_transaction']($type);
+	}
+
+	public function get_version(): string
+	{
+		return Utils::$smcFunc['db_get_version']();
 	}
 }
