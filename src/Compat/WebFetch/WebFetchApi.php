28a27cce8fda8e0c35aee64674053f46fb86c791
Bugo <cheetah@bk.ru>
2025-01-26 15:58:01 +0500

    Update WebFetchApi

--- src/Compat/WebFetch/WebFetchApi.php
+++ src/Compat/WebFetch/WebFetchApi.php
@@ -10,12 +10,14 @@
 
 namespace Bugo\Compat\WebFetch;
 
+use Bugo\Compat\Url;
+
 use function fetch_web_data;
 
-class WebFetchApi
+abstract class WebFetchApi
 {
-	public static function fetch(string $url): bool|string
+    public static function fetch(Url|string $url, string|array $post_data = [], bool $keep_alive = false): string|false
 	{
-		return fetch_web_data($url);
+		return fetch_web_data($url, $post_data, $keep_alive);
 	}
 }
